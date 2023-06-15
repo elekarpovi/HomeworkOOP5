dba7f2fb7bbd07f867bd6f6ee96e3cc150af6af6
Elena Karpovich <elekarpovi@tut.by>
2023-06-16 00:04:26 +0300

    Added presenters

---
+++ presenters/Model.java
@@ -0,0 +1,19 @@
+package presenters;
+
+import HomeworkOOP5.models.Table;
+
+import java.util.Collection;
+import java.util.Date;
+
+public interface Model {
+    Collection<Table> loadTables();
+
+    /**
+     * Бронирование столика
+     * @param reservationDate Дата бронирования
+     * @param tableNo Номер столика
+     * @param name Имя клиента
+     */
+    int reservationTable(Date reservationDate, int tableNo, String name);
+    int changeReservationTable(int oldReservationId, Date reservationDate, int tableNo, String name);
+}
